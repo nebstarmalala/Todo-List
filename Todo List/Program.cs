82e6120ed4fad354dfeea8fe2505ea1c3a91e7ec
nebstarmalala <nebstartrizzle@gmail.com>
2023-03-14 14:57:32 +0300

    add try block for exception handling

--- Todo List/Program.cs
+++ Todo List/Program.cs
@@ -11,18 +11,23 @@ namespace Todo_List
         static void Main(string[] args)
         {
             Console.WriteLine("========== Welcome to myToDo list ==========");
-            Console.Write("Enter task: ");
-            string taskname = Console.ReadLine();
-            Console.Write("Enter Importance (Critical, Important, Minor): ");
-            string importance = Console.ReadLine();
+            try
+            {
+                Console.Write("Enter task: ");
+                string taskname = Console.ReadLine();
+                Console.Write("Enter Importance (Critical, Important, Minor): ");
+                string importance = Console.ReadLine();
 
-            Task task = new Task(taskname, importance);
-            Console.WriteLine("New Task added");
-            Console.WriteLine($"Task: {task.taskname}");
-            Console.WriteLine($"Importance: {task.Importance}");
-            Console.WriteLine($"Time Added: {task.timeAdded}");
-
-
+                Task task = new Task(taskname, importance);
+                Console.WriteLine("New Task added");
+                Console.WriteLine($"Task: {task.taskname}");
+                Console.WriteLine($"Importance: {task.Importance}");
+                Console.WriteLine($"Time Added: {task.timeAdded}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
         }
