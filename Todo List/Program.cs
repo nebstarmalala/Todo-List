0daefd1e1d4983cc16fbf0df206414db905edd69
nebstarmalala <nebstartrizzle@gmail.com>
2023-03-14 14:55:11 +0300

    create an interacting shell for the to do list

--- Todo List/Program.cs
+++ Todo List/Program.cs
@@ -10,6 +10,21 @@ namespace Todo_List
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("========== Welcome to myToDo list ==========");
+            Console.Write("Enter task: ");
+            string taskname = Console.ReadLine();
+            Console.Write("Enter Importance (Critical, Important, Minor): ");
+            string importance = Console.ReadLine();
+
+            Task task = new Task(taskname, importance);
+            Console.WriteLine("New Task added");
+            Console.WriteLine($"Task: {task.taskname}");
+            Console.WriteLine($"Importance: {task.Importance}");
+            Console.WriteLine($"Time Added: {task.timeAdded}");
+
+
+
+            Console.ReadLine();
         }
     }
 }
