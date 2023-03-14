f6a1fc8f55dc51dcc107a3b61bed15f6140b884c
nebstarmalala <nebstartrizzle@gmail.com>
2023-03-14 14:04:01 +0300

    create task datatype plus its constructor

---
+++ Todo List/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_List
+{
+    class Task
+    {
+        public string taskname;
+        public string importance;
+        public DateTime timeAdded;
+
+        public Task(string Taskname, string Importance)
+        {
+            taskname = Taskname;
+            importance = Importance;
+            timeAdded = DateTime.Now;
+        }
+    }
+}
